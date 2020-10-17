b45eebef8c4fb5a52f0b98bccc9de887a47ac6ea
flatronsamatron <flatronsamatron@gmail.com>
2020-10-17 13:05:40 +0300

    add build

---
+++ build/precache-manifest.0bd3cdce74de6308fb29d97acf77f9a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d598783f325526a341f5f2828a8577fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6884b651307d33c476ed",
+    "url": "/static/css/main.2b8aed40.chunk.css"
+  },
+  {
+    "revision": "c6e149df4c668c9dd3b9",
+    "url": "/static/js/2.a55a8bb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a55a8bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6884b651307d33c476ed",
+    "url": "/static/js/main.04919cd2.chunk.js"
+  },
+  {
+    "revision": "d2ca2d8e9eceb80b7104",
+    "url": "/static/js/runtime-main.2d85e0d0.js"
+  }
+]);
